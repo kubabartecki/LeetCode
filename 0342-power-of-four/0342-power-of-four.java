32f589720cbe18e4e5e7c69d20507318700cc0f1
Jakub Bartecki <94709080+kubabartecki@users.noreply.github.com>
2023-10-24 01:21:18 +0200

    Time: 1 ms (76.18%), Space: 39.8 MB (25.61%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        while(n > 1 && (n & 0b11) == 0){
+        if(n <= 0) return false;
+        while((n & 0b11) == 0){
             n >>= 2;
         }
         return n == 1 ? true : false;
