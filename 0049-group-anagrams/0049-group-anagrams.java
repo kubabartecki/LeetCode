6378b5c90640e2c3cf071109c50cc31564005bc1
Jakub Bartecki <94709080+kubabartecki@users.noreply.github.com>
2024-02-07 12:09:35 +0100

    Time: 10 ms (32.12%), Space: 48.6 MB (10.06%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,11 +2,23 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> anagramGroup = new HashMap<>();
         for (String anagram: strs) {
-            String sortedLetters = anagram.chars().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-            .toString();
-            anagramGroup.computeIfAbsent(sortedLetters, sL -> new ArrayList<>());
-            anagramGroup.get(sortedLetters).add(anagram);
+            String anagramCode = sortedStringCode(anagram);
+            anagramGroup.computeIfAbsent(anagramCode, sL -> new ArrayList<>());
+            anagramGroup.get(anagramCode).add(anagram);
         }
         return new ArrayList<>(anagramGroup.values());
+    }
+    
+    public String sortedStringCode(String str) {
+        int count[] = new int[26];
+        for (char letter: str.toCharArray()) {
+            ++count[letter - 'a'];
+        }
+        StringBuilder strB = new StringBuilder();
+        for (int i = 0; i < 26; ++i) {
+            if (count[i] == 0) continue;
+            strB.append((char)'a' + i).append(count[i]);
+        }
+        return strB.toString();
     }
 }
