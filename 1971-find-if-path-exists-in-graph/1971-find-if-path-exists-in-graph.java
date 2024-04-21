3540bbf8c54d82c0640443cc585e2a654a42f3df
Jakub Bartecki <94709080+kubabartecki@users.noreply.github.com>
2024-04-21 22:11:58 +0200

    Time: 9 ms (98.20%), Space: 109.3 MB (93.45%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,32 +1,26 @@
 class Solution {
+    private int [] root;
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        Map<Integer, List<Integer>> connections = new HashMap<>();
-        List<Integer> toVisit = new ArrayList<>();
-        List<Integer> visited = new ArrayList<>();
-
+        root = new int [n];
+        for (int i = 0; i < n; ++i) {
+            root[i] = i;
+        }
         for (int [] edge : edges) {
-            connections.putIfAbsent(edge[0], new ArrayList<Integer>());
-            connections.putIfAbsent(edge[1], new ArrayList<Integer>());
-            connections.get(edge[0]).add(edge[1]);
-            connections.get(edge[1]).add(edge[0]);
+            union(edge[0], edge[1]);
         }
+        return find(source) == find(destination);
+    }
 
-        toVisit.add(source);
-        while (!toVisit.isEmpty()) {
-            int vertice = toVisit.remove(0);
-            if (visited.contains(vertice)) {
-                // in the meanwhile other vertice could check that one
-                continue;
-            }
-            visited.add(vertice);
-            if (vertice == destination) {
-                return true;
-            }
-            toVisit.addAll(
-                connections.getOrDefault(vertice, Collections.emptyList()).stream()
-                    .filter(a -> !visited.contains(a)).collect(Collectors.toList())
-            );
+    private int find (int x) {
+        if (x == root[x]) {
+            return x;
         }
-        return false;
+        return root[x] = find(root[x]);
+    }
+
+    private void union (int x, int y) {
+        x = find(x);
+        y = find(y);
+        root[y] = x;
     }
 }
