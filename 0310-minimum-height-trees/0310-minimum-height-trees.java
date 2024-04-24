64ec612130adf85384c4bd1b96e0d17f61556780
Jakub Bartecki <94709080+kubabartecki@users.noreply.github.com>
2024-04-24 11:25:36 +0200

    Time: 15 ms (92.48%), Space: 55.8 MB (65.88%) - LeetHub

---
+++ 0310-minimum-height-trees/0310-minimum-height-trees.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        List<Integer> mht = new ArrayList<>();
+        if (n == 1) {
+            mht.add(0);
+            return mht;
+        }
+        List<List<Integer>> tree = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            tree.add(new ArrayList<Integer>());
+        }
+        int [] degree = new int [n];
+        for (int [] edge : edges) {
+            tree.get(edge[0]).add(edge[1]);
+            tree.get(edge[1]).add(edge[0]);
+            ++degree[edge[0]];
+            ++degree[edge[1]];
+        }
+
+        Queue<Integer> leaves = new ArrayDeque<>();
+        for (int i = 0; i < n; ++i) {
+            if (degree[i] == 1) {
+                leaves.offer(i);
+            }
+        }
+        int used = 0;
+        while (n - used > 2) {
+            int oldSize = leaves.size();
+            used += oldSize;
+            for (int i = 0; i < oldSize; ++i) {
+                int leaf = leaves.poll();
+                for (int node : tree.get(leaf)) {
+                    if (--degree[node] == 1) {
+                        leaves.offer(node);
+                    }
+                }
+            }
+        }
+        mht.addAll(leaves);
+        return mht;
+    }
+}
